22b9b45e94e4aebce522caee95abf7e9ce0fd52d
BaekGyungSun <73513929+BaekGyungSun@users.noreply.github.com>
2021-11-09 23:09:01 +0900

    Add files via upload

---
+++ 9498.py
@@ -0,0 +1,13 @@
+g = int(input())
+
+if 0 <= g <= 100:
+    if g >= 90:
+        print('A')
+    elif g >= 80:
+        print('B')
+    elif g >= 70:
+        print('C')
+    elif g >= 60:
+        print('D')
+    else:
+        print('F')
