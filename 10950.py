816540e741b9c4e3ebb4d6e75364619790b8f30b
BaekGyungSun <73513929+BaekGyungSun@users.noreply.github.com>
2021-11-09 23:39:59 +0900

    Add files via upload

---
+++ 10950.py
@@ -0,0 +1,6 @@
+T = int(input())
+
+for k in range(T):
+    A, B = input().split(' ')
+    A, B = int(A), int(B)
+    print(A+B)
---
+++ 8393.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+sum = 0
+for k in range(n+1):
+    sum += k
+print(sum)
