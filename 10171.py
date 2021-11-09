c8dabad6442fdbe65782cb87da7f3075268d1baf
BaekGyungSun <73513929+BaekGyungSun@users.noreply.github.com>
2021-11-09 22:33:29 +0900

    Add files via upload

---
+++ 10171.py
@@ -0,0 +1,12 @@
+a = "\\"
+b = "/"
+c = ")"
+d = "("
+e = "'"
+f = "__"
+g = "|"
+
+print('{}    {}{}'.format(a,b,a))
+print(' {}  {} {}{}'.format(c,d,e,c))
+print('{}  {}  {}'.format(d,b,c))
+print(' {}{}{}{}{}'.format(a,d,f,c,g))
