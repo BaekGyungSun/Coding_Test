4aa99f4472a3a1503e7039867c76d6daf57fabdc
BaekGyungSun <73513929+BaekGyungSun@users.noreply.github.com>
2021-11-09 22:33:40 +0900

    Add files via upload

---
+++ 10172.py
@@ -0,0 +1,17 @@
+a = "|"
+b = "\\"
+c = "_"
+d = "/"
+e = "("
+f = ")"
+g = "\""
+h = "}"
+i = "`"
+j = "^"
+k = "="
+
+print('{}{}{}{}{}'.format(a,b,c,d,a))
+print('{}q p{}   {}{}'.format(a,a,d,h))
+print('{} 0 {}{}{}{}{}'.format(e,f,g,g,g,b))
+print('{}{}{}{}{}    {}'.format(a,g,j,g,i,a))
+print('{}{}{}{}{}{}{}{}{}{}'.format(a,a,c,d,k,b,b,c,c,a))
